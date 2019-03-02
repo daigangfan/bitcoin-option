113b1f5302b2f82f58db576b2688232071e71fd9
daigangfan <daigf.15@sem.tsinghua.edu.cn>
2019-03-02 10:39:48 +0800

    add more describe

--- describe.py
+++ describe.py
@@ -4,13 +4,14 @@ import datetime
 import pandas as pd
 
 price_result = pd.read_excel("price_result.xlsx")
+price_result["S/X"] = price_result["spot_price"] / price_result["strike"]
 btc_data = pd.read_excel("btc_data.xlsx")
 
 
 def get_time(x):
     start_time = x["date"]
     end_time = x["exp_date"]
-    return (end_time - start_time).days+1
+    return (end_time - start_time).days + 1
 
 
 price_result["time"] = price_result.apply(get_time, axis=1)
@@ -32,13 +33,25 @@ btc_describe.loc["count"] = btc_describe.loc["count"].astype(int)
 with open("describe_btc_data.tex", "w") as f:
     f.write(btc_describe.to_latex(float_format="{:.2f}".format))
 
+option_initial_data = price_result.groupby("contract_label").first()
 
-option_initial_data=price_result.groupby("contract_label").first()
+option_describe = option_initial_data[["time", "strike"]].describe()
 
-option_describe=option_initial_data[["time","strike"]].describe()
+option_describe.loc["count", "call_number"] = 165
+option_describe.loc["count", "put_number"] = 91
 
-option_describe.loc["count","call_number"]=165
-option_describe.loc["count","put_number"]=91
-
-with open("describe_option_data.tex","w") as f:
+with open("describe_option_data.tex", "w") as f:
     f.write(option_describe.to_latex(float_format="{:.2f}".format))
+
+writer = pd.ExcelWriter("price_result.xlsx")
+with writer:
+    price_result.to_excel(writer)
+
+price_result["time_cut"] = pd.cut(
+    price_result["time"], [0, 20, 80, 180, 637], right=False)
+price_result["strike_cut"] = pd.cut(
+    price_result["strike"], [0, 0.6, 0.8, 1.2, 3.79], right=False)
+
+price_biases = price_result.filter(regex="bias_int\d+", axis=1)
+price_result[["abs_bias_int5", "abs_bias_int10", "abs_bias_int20",
+              "abs_bias_int30"]] = price_biases.applymap(lambda x: abs(x))
